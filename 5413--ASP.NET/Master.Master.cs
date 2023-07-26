806376504edb0da5910eb88143e50a9baff99031
Eduardo Pereira <119636074+EduardoPereira7@users.noreply.github.com>
2023-07-26 23:57:38 +0100

    nav

--- 5413--ASP.NET/Master.Master.cs
+++ 5413--ASP.NET/Master.Master.cs
@@ -11,7 +11,26 @@ namespace _5413__ASP.NET
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                /* Verificar se o utilizador está logado
+                if ()
+                
+                    // Utilizador com login, esconder "Login" e "Sign up", mostrar "Log Out"
+                    loginLink.Visible = false;
+                    signUpLink.Visible = false;
+                    logoutLink.Visible = true;
+                }
+                else
+                {
+                */
+                    // Utilizador sem login, esconder "Log Out", mostrar "Login" e "Sign up"
+                    loginLink.Visible = true;
+                    signUpLink.Visible = true;
+                    logoutLink.Visible = false;
+                //}
 
+            }
         }
     }
 }
