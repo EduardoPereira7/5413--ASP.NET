57495454fadfb1913ddfb7b21fe394aca2b2843f
Eduardo Pereira <119636074+EduardoPereira7@users.noreply.github.com>
2023-09-08 23:22:19 +0100

    On Cascate

--- 5413--ASP.NET/DAL/DAL.cs
+++ 5413--ASP.NET/DAL/DAL.cs
@@ -9,7 +9,7 @@ namespace _5413__ASP.NET.DAL
 {
     public class DAL
     {
-        private string connString = @"Data Source=MSIGAMINGPLUS;Initial Catalog=DB;Integrated Security=True;";
+        private string connString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=blog;Integrated Security=True;";
 
         public string getconnString()
         {
